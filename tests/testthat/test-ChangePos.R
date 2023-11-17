7434706c7126ec44261c7d092f57a68952a4f4b1
TJHeaton <t.heaton@leeds.ac.uk>
2023-11-17 15:03:43 +0000

    Testing ChangePos against legacy code

--- tests/testthat/test-ChangePos.R
+++ tests/testthat/test-ChangePos.R
@@ -49,3 +49,58 @@ test_that("ChangePos gives expected outcomes", {
 
 })
 
+
+### Second test
+test_that("ChangePos gives sameas legacy code", {
+
+  set.seed(14)
+  rate_s <- initial_rate_s <- c(0, 2, 5, 7, 10)
+  rate_h <- initial_rate_h <- c(5, 2, 4, 6)
+  n_changepoints <- length(rate_s)
+  integrated_rate <- initial_integrate_rate <- .FindIntegral(
+    initial_rate_s,
+    initial_rate_h)
+
+  calendar_ages <- stats::runif(
+    200,
+    min = min(initial_rate_s),
+    max = max(initial_rate_s))
+
+  set.seed(11)
+  for(i in 1:1000) {
+    return_val <- .ChangePos(
+      calendar_ages = calendar_ages,
+      rate_s = rate_s,
+      rate_h = rate_h,
+      integrated_rate = integrated_rate)
+
+    rate_s <- revised_rate_s <- return_val$s
+    integrated_rate <- revised_integrated_rate <- return_val$integrated_rate
+  }
+
+  set.seed(11)
+  source(test_path("fixtures", "LegacyChangePos.R"))
+  # Reset the starting values
+  rate_h <- initial_rate_h
+  rate_s <- initial_rate_s
+  integrated_rate <- initial_integrate_rate
+  for(i in 1:1000) {
+    return_val <- LegacyChangePos(
+      th = calendar_ages,
+      s = rate_s,
+      h = rate_h,
+      intrate = integrated_rate)
+
+    rate_s <- legacy_rate_s <- return_val$s
+    integrated_rate <- legacy_integrated_rate <- return_val$intrate
+  }
+
+  # Test revised code gives same rate_s after running multiple time
+  expect_identical(revised_rate_s, legacy_rate_s)
+
+  # Test revised code gives same integrated rate after running multiple time
+  expect_identical(revised_integrated_rate, legacy_integrated_rate)
+
+})
+
+
