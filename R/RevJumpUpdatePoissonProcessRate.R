c72a361c5a4794f697e6b2e33e4599d91506e4c9
TJHeaton <t.heaton@leeds.ac.uk>
2023-11-23 16:10:16 +0000

    Creating update of PP step

---
+++ R/RevJumpUpdatePoissonProcessRate.R
@@ -0,0 +1,14 @@
+RevJumpUpdatePoissonProcessRate <- function(
+    theta,
+    rate_s,
+    rate_h,
+    integrated_rate,
+    prior_h_shape,
+    prior_h_rate,
+    n_changepoint_lamnda,
+    prob_move) {
+
+  # Empty function
+  print("Hello!")
+
+}
