8b49a907bf430ebc07cd7480dae2b83af0552891
TJHeaton <t.heaton@leeds.ac.uk>
2023-11-30 15:51:00 +0000

    Editing the legacy code for FindCalPrior to give 0 at final changepoint

--- tests/testthat/fixtures/LegacyFindCalPrior.R
+++ tests/testthat/fixtures/LegacyFindCalPrior.R
@@ -2,7 +2,7 @@
 LegacyFindCalPrior <- function(s, h, t) {
   NumChange <- length(s) - 2
   # Prior is proportional to rate at given calendar age
-  priort <- approx(x = s[1:(NumChange+2)], y = h[c(1:(NumChange+1), NumChange+1)], xout = t, method = "constant")$y
+  priort <- approx(x = s[1:(NumChange+2)], y = c(h[1:(NumChange+1)], 0), xout = t, method = "constant")$y
   return(priort)
 }
 
