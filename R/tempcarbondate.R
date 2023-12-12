4d06a37e2b9f0549a82e9f45b9cce373b4a373d5
TJHeaton <t.heaton@leeds.ac.uk>
2023-12-12 15:31:11 +0000

    Adding hidden functions from carbondate

--- R/tempcarbondate.R
+++ R/tempcarbondate.R
@@ -35,3 +35,30 @@
   data$c14_sig <- 8033 * data$f14c_sig / data$f14c
   return(data)
 }
+
+.AddF14cColumns <- function(data) {
+  if (any(names(data) == "f14c") && any(names(data) == "f14c_sig")) {
+    return(data)
+  }
+  data$f14c <- exp(-data$c14_age / 8033)
+  data$f14c_sig <- data$f14c * data$c14_sig / 8033
+  return(data)
+}
+
+
+.Convert14CageToF14c <- function(c14_determinations, c14_sigmas) {
+
+  f14c <- exp(-c14_determinations / 8033)
+  f14c_sig <- f14c * c14_sigmas / 8033
+
+  return(data.frame(f14c = f14c, f14c_sig = f14c_sig))
+}
+
+
+.ConvertF14cTo14Cage <- function(f14c_determinations, f14c_sigmas) {
+
+  c14_age <- -8033 * log(f14c_determinations)
+  c14_sig <- 8033 * f14c_sigmas / f14c_determinations
+
+  return(data.frame(c14_age = c14_age, c14_sig = c14_sig))
+}
